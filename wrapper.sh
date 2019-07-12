cccd2b80456fd2f96d40a788584eff34ff334983
mrbulldops <19413126+mrbulldops@users.noreply.github.com>
2019-07-12 11:45:25 -0700

    revert testing

--- wrapper.sh
+++ wrapper.sh
@@ -2,5 +2,6 @@
 
 echo "Nginx is running..."
 
-#exec nginx -g "daemon off;" #TESTING ALTERNATIVE LINE
-RUN echo "daemon off;" >> /etc/nginx/nginx.conf
+exec nginx -g "daemon off;" 
+#TESTING ALTERNATIVE 
+#RUN echo "daemon off;" >> /etc/nginx/nginx.conf
